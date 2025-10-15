da1893752dca118eaee5f3134f802a74f30980f1
LeonardoBruchez <bruchezleonardo@gmail.com>
2025-10-15 14:44:28 -0300

    adjust styles and script

---
+++ script.js
@@ -0,0 +1,13 @@
+function calcular(){
+  var txtv = document.querySelector('input#txtvel')
+  var res = document.querySelector('div#res')
+  var vel = Number(txtv.value)
+  res.innerHTML = `<p>Sua velocidade atual é de <strong>${vel} Km/h</strong></p>`
+
+  if (vel > 60){
+    res.innerHTML += `<p>Você foi <strong>MULTADO</strong> por excesso de velocidade</p>`
+  }else{
+    res.innerHTML += `<p>Tenha uma ótima viagem!</p>`
+  }
+  res.innerHTML += `<p>Dirija sempre com cinto de segurança!</p>`
+}
