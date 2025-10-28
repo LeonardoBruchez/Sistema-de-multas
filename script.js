55ffef233fc0bf0cfa3e22097c47dc98824d8d5b
LeonardoBruchez <bruchezleonardo@gmail.com>
2025-10-28 15:51:24 -0300

    adjust the limit above speed

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function calcular(){
   var txtv = document.querySelector('input#txtvel')
   var res = document.querySelector('div#res')
+  var main = document.querySelector('main#main')
+  var submit = document.querySelector('input#buttonVerify')
+  var velLimit = document.querySelector('h3#velLimit')
   res.style.textAlign = 'center'
   res.style.marginBlock = '3rem'
   res.style.font = '400 0.8rem arial'
@@ -9,8 +12,18 @@ function calcular(){
 
   if (vel > 60){
     res.innerHTML += `<p>Você foi <strong>MULTADO</strong> por excesso de velocidade</p>`
+    res.style.font = '500 1.3rem arial'
+    txtv.style.outline = '2px solid red'
+    main.style.background = 'rgb(192, 89, 89)'
+    document.body.style.background = 'rgba(214, 169, 169, 1)'
+    main.style.border = 'none'
+    submit.style.outline = '2px solid red'
+    velLimit.classList.remove('displayNone')
+    
+
   }else{
     res.innerHTML += `<p>Tenha uma ótima viagem!</p>`
+    res.style.font = '500 1.3rem arial'
   }
   res.innerHTML += `<p>Dirija sempre com cinto de segurança!</p>`
 }
